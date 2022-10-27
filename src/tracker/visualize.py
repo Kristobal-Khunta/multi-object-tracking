9d57856fceaa1d4e267cf9942942da3e8ba051f5
Maxim Goldin <maxim.goldin@cmai.team>
2022-10-27 14:14:20 +0300

    creating gifs with the results of the tracker

---
+++ src/tracker/visualize.py
@@ -0,0 +1,284 @@
+import os
+import random
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import motmetrics as mm
+import numpy as np
+import torch
+from cycler import cycler as cy
+from torchvision.transforms import functional as F
+from tqdm.auto import tqdm
+import time
+import copy
+import gif
+
+
+colors = [
+    "aliceblue",
+    "antiquewhite",
+    "aqua",
+    "aquamarine",
+    "azure",
+    "beige",
+    "bisque",
+    "black",
+    "blanchedalmond",
+    "blue",
+    "blueviolet",
+    "brown",
+    "burlywood",
+    "cadetblue",
+    "chartreuse",
+    "chocolate",
+    "coral",
+    "cornflowerblue",
+    "cornsilk",
+    "crimson",
+    "cyan",
+    "darkblue",
+    "darkcyan",
+    "darkgoldenrod",
+    "darkgray",
+    "darkgreen",
+    "darkgrey",
+    "darkkhaki",
+    "darkmagenta",
+    "darkolivegreen",
+    "darkorange",
+    "darkorchid",
+    "darkred",
+    "darksalmon",
+    "darkseagreen",
+    "darkslateblue",
+    "darkslategray",
+    "darkslategrey",
+    "darkturquoise",
+    "darkviolet",
+    "deeppink",
+    "deepskyblue",
+    "dimgray",
+    "dimgrey",
+    "dodgerblue",
+    "firebrick",
+    "floralwhite",
+    "forestgreen",
+    "fuchsia",
+    "gainsboro",
+    "ghostwhite",
+    "gold",
+    "goldenrod",
+    "gray",
+    "green",
+    "greenyellow",
+    "grey",
+    "honeydew",
+    "hotpink",
+    "indianred",
+    "indigo",
+    "ivory",
+    "khaki",
+    "lavender",
+    "lavenderblush",
+    "lawngreen",
+    "lemonchiffon",
+    "lightblue",
+    "lightcoral",
+    "lightcyan",
+    "lightgoldenrodyellow",
+    "lightgray",
+    "lightgreen",
+    "lightgrey",
+    "lightpink",
+    "lightsalmon",
+    "lightseagreen",
+    "lightskyblue",
+    "lightslategray",
+    "lightslategrey",
+    "lightsteelblue",
+    "lightyellow",
+    "lime",
+    "limegreen",
+    "linen",
+    "magenta",
+    "maroon",
+    "mediumaquamarine",
+    "mediumblue",
+    "mediumorchid",
+    "mediumpurple",
+    "mediumseagreen",
+    "mediumslateblue",
+    "mediumspringgreen",
+    "mediumturquoise",
+    "mediumvioletred",
+    "midnightblue",
+    "mintcream",
+    "mistyrose",
+    "moccasin",
+    "navajowhite",
+    "navy",
+    "oldlace",
+    "olive",
+    "olivedrab",
+    "orange",
+    "orangered",
+    "orchid",
+    "palegoldenrod",
+    "palegreen",
+    "paleturquoise",
+    "palevioletred",
+    "papayawhip",
+    "peachpuff",
+    "peru",
+    "pink",
+    "plum",
+    "powderblue",
+    "purple",
+    "rebeccapurple",
+    "red",
+    "rosybrown",
+    "royalblue",
+    "saddlebrown",
+    "salmon",
+    "sandybrown",
+    "seagreen",
+    "seashell",
+    "sienna",
+    "silver",
+    "skyblue",
+    "slateblue",
+    "slategray",
+    "slategrey",
+    "snow",
+    "springgreen",
+    "steelblue",
+    "tan",
+    "teal",
+    "thistle",
+    "tomato",
+    "turquoise",
+    "violet",
+    "wheat",
+    "white",
+    "whitesmoke",
+    "yellow",
+    "yellowgreen",
+]
+
+
+def plot_sequence(tracks, db, first_n_frames=None):
+    """Plots a whole sequence
+
+    Args:
+        tracks (dict): The dictionary containing the track dictionaries in the form tracks[track_id][frame] = bb
+        db (torch.utils.data.Dataset): The dataset with the images belonging to the tracks (e.g. MOT_Sequence object)
+    """
+
+    # print("[*] Plotting whole sequence to {}".format(output_dir))
+
+    # if not osp.exists(output_dir):
+    # 	os.makedirs(output_dir)
+
+    # infinite color loop
+    cyl = cy("ec", colors)
+    loop_cy_iter = cyl()
+    styles = defaultdict(lambda: next(loop_cy_iter))
+
+    for i, v in enumerate(db):
+        img = v["img"].mul(255).permute(1, 2, 0).byte().numpy()
+        width, height, _ = img.shape
+
+        dpi = 96
+        fig, ax = plt.subplots(1, dpi=dpi)
+        fig.set_size_inches(width / dpi, height / dpi)
+        ax.set_axis_off()
+        ax.imshow(img)
+
+        for j, t in tracks.items():
+            if i in t.keys():
+                t_i = t[i]
+                ax.add_patch(
+                    plt.Rectangle(
+                        (t_i[0], t_i[1]),
+                        t_i[2] - t_i[0],
+                        t_i[3] - t_i[1],
+                        fill=False,
+                        linewidth=1.0,
+                        **styles[j],
+                    )
+                )
+
+                ax.annotate(
+                    j,
+                    (
+                        t_i[0] + (t_i[2] - t_i[0]) / 2.0,
+                        t_i[1] + (t_i[3] - t_i[1]) / 2.0,
+                    ),
+                    color=styles[j]["ec"],
+                    weight="bold",
+                    fontsize=6,
+                    ha="center",
+                    va="center",
+                )
+
+        plt.axis("off")
+        # plt.tight_layout()
+        plt.show()
+        # plt.savefig(im_output, dpi=100)
+        # plt.close()
+
+        if first_n_frames is not None and first_n_frames - 1 == i:
+            break
+
+
+@gif.frame
+def plot_single_tracked_frame(img, img_idx, tracks):
+    cyl = cy("ec", colors)
+    loop_cy_iter = cyl()
+    styles = defaultdict(lambda: next(loop_cy_iter))
+
+    dpi = 300
+    fig, ax = plt.subplots(1, dpi=dpi)
+    # fig.set_size_inches(width / dpi, height / dpi)
+    ax.set_axis_off()
+    ax.imshow(img)
+
+    for j, t in tracks.items():
+        if img_idx in t.keys():
+            t_i = t[img_idx]
+            ax.add_patch(
+                plt.Rectangle(
+                    (t_i[0], t_i[1]),
+                    t_i[2] - t_i[0],
+                    t_i[3] - t_i[1],
+                    fill=False,
+                    linewidth=1.0,
+                    **styles[j],
+                )
+            )
+
+            ax.annotate(
+                j,
+                (
+                    t_i[0] + (t_i[2] - t_i[0]) / 2.0,
+                    t_i[1] + (t_i[3] - t_i[1]) / 2.0,
+                ),
+                color=styles[j]["ec"],
+                weight="bold",
+                fontsize=6,
+                ha="center",
+                va="center",
+            )
+
+
+def collect_frames_for_gif(sequence, tracker_seq_res, first_n_frames=None):
+    frames = []
+    total_length = first_n_frames if first_n_frames is not None else len(sequence)
+    for img_idx, data in tqdm(enumerate(sequence), total=total_length):
+        img = data["img"].mul(255).permute(1, 2, 0).byte().numpy()
+
+        frame = plot_single_tracked_frame(img, img_idx, tracker_seq_res)
+        frames.append(frame)
+        if first_n_frames is not None and first_n_frames - 1 == i:
+            break
+    return frames
