c8572580a60c0a3bc44a879cec682456b8ee4e80
Kristobal Khunta <maxim.goldin@cmai.team>
2022-10-28 16:30:15 +0300

    refactor reid

--- src/mot/models/reid_losses.py
+++ src/mot/models/reid_losses.py
@@ -56,3 +56,28 @@ class HardBatchMiningTripletLoss(torch.nn.Module):
         # loss = max(0, -1*(neg_dist - pos_dist) + margin)
         y = torch.ones_like(distance_negative_pairs)
         return self.ranking_loss(distance_negative_pairs, distance_positive_pairs, y)
+
+
+class CombinedLoss(object):
+    def __init__(self, margin=0.3, weight_triplet=1.0, weight_ce=1.0):
+        super(CombinedLoss, self).__init__()
+        self.triplet_loss = HardBatchMiningTripletLoss()
+        self.cross_entropy = torch.nn.CrossEntropyLoss()
+        self.weight_triplet = weight_triplet
+        self.weight_ce = weight_ce
+
+    def __call__(self, logits, features, gt_pids):
+        loss = 0.0
+        loss_summary = {}
+        if self.weight_triplet > 0.0:
+            loss_t = self.triplet_loss(features, gt_pids) * self.weight_triplet
+            loss += loss_t
+            loss_summary["Triplet Loss"] = loss_t
+
+        if self.weight_ce > 0.0:
+            loss_ce = self.cross_entropy(logits, gt_pids) * self.weight_ce
+            loss += loss_ce
+            loss_summary["CE Loss"] = loss_ce
+
+        loss_summary["Loss"] = loss
+        return loss, loss_summary
