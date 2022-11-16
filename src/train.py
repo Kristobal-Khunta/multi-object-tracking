c54db048546523ba35eabafc961e022d65585bee
Kristobal Khunta <maxim.goldin@cmai.team>
2022-11-16 17:17:18 +0300

    add reid triplet mode

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 
 import motmetrics as mm
@@ -5,16 +6,14 @@ import numpy as np
 import torch
 from scipy.optimize import linear_sum_assignment as linear_assignment
 
-from market import  utils
-
-# Load helper code
+from market import utils
 from market.datamanager import ImageDataManager
 from market.eval import evaluate
 from market.models import build_model
-import argparse
-mm.lap.default_solver = "lap"
-from mot.utils import euclidean_squared_distance, cosine_distance
+from mot.models.reid_losses import CombinedLoss
+from mot.utils import cosine_distance, euclidean_squared_distance
 
+mm.lap.default_solver = "lap"
 
 
 def set_all_seeds(seed):
@@ -28,9 +27,17 @@ def set_all_seeds(seed):
 def setup_parser():
     """Set up Python's ArgumentParser with data, model, trainer, and other arguments."""
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("--model_head_type", type=str, default='softmax',choices=['softmax', 'triplet'] )
+    parser.add_argument(
+        "--model_type", type=str, default="softmax", choices=["softmax", "triplet"]
+    )
 
-    parser.add_argument("--metric_fn", type=str, default= 'cosine', help="metric_fn")
+    parser.add_argument(
+        "--metric_fn",
+        type=str,
+        default="cosine",
+        choices=["cosine", "euclidian"],
+        help="metric_fn",
+    )
     parser.add_argument("--max_epoch", type=int, default=30)
     parser.add_argument("--epoch_eval_freq", type=int, default=5)
     parser.add_argument("--print_freq", type=int, default=50)
@@ -39,16 +46,22 @@ def setup_parser():
 
 
 name2metricfn = {
-    'cosine':cosine_distance,
-    'euclidian':euclidean_squared_distance,
+    "cosine": cosine_distance,
+    "euclidian": euclidean_squared_distance,
 }
+
 
 def main():
     set_all_seeds(12347)
     parser = setup_parser()
     args = parser.parse_args()
-    
-    reid_root_dir = ".."
+
+    model_type = args.model_type
+    from pathlib import Path
+
+    reid_root_dir = Path(__file__).parent.parent
+    print("reid_root_dir", reid_root_dir)
+    reid_root_dir = str(reid_root_dir)
 
     ### DATA ###
     datamanager = ImageDataManager(
@@ -63,8 +76,12 @@ def main():
     test_loader = datamanager.test_loader
 
     #### Define model ####
+
     model = build_model(
-        "resnet34", datamanager.num_train_pids, loss=args.model_head_type, pretrained=True
+        "resnet34",
+        datamanager.num_train_pids,
+        loss=model_type,
+        pretrained=True,
     )
     model = model.cuda()
 
@@ -76,9 +93,9 @@ def main():
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10)
 
     #### criterion #####
-    if args.model_head_type == 'softmax':
+    if model_type == "softmax":
         criterion = torch.nn.CrossEntropyLoss()
-    if args.model_head_type == 'triplet':
+    if model_type == "triplet":
         criterion = CombinedLoss(0.3, 1.0, 1.0)
 
     ##### metric_fn #####
@@ -91,41 +108,36 @@ def main():
         batch_time = utils.AverageMeter()
         end = time.time()
         model.train()
-        
+
         for batch_idx, data in enumerate(train_loader):
             # Predict output.
             imgs, pids = data["img"].cuda(), data["pid"].cuda()
-            output = model(imgs)
-            # Compute loss.
-            loss = criterion(output, pids)
+            if model_type == "softmax":
+                output = model(imgs)
+                loss = criterion(output, pids)
+                loss_summary = {"Loss": loss}
+
+            if model_type == "triplet":
+                logits, features = model(imgs)
+                loss, loss_summary = criterion(logits, features, pids)
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
 
             batch_time.update(time.time() - end)
-            losses.update({"Loss": loss})
+            losses.update(loss_summary)
             if (batch_idx + 1) % args.print_freq == 0:
                 utils.print_statistics(
                     batch_idx, num_batches, epoch, args.max_epoch, batch_time, losses
                 )
             end = time.time()
-        
+
         if (epoch + 1) % args.epoch_eval_freq == 0 or epoch == args.max_epoch - 1:
             rank1, mAP = evaluate(model, test_loader, metric_fn=metric_fn)
-            print(
-                "Epoch {0}/{1}: Rank1: {rank}, mAP: {map}".format(
-                    epoch + 1, args.max_epoch, rank=rank1, map=mAP
-                )
-            )
+            print(f"Epoch {epoch + 1}/{args.max_epoch}: Rank1: {rank1}, mAP: {mAP}")
         scheduler.step()
-        
-        
 
 
 if __name__ == "__main__":
     main()
-
-
-
